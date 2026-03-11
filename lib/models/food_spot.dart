a33ed07113720136331e04ead8b875002e053cfd
Kayla Thornton <kaylathornton55@icloud.com>
2026-03-11 10:49:50 -0400

    initialize: food_spots model init for database use

---
+++ lib/models/food_spot.dart
@@ -0,0 +1,21 @@
+/* 
+  Author - Kayla Thornton
+  Purpose - Define structure for Food Spots database
+ */
+class FoodSpot {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final int hours;
+  final int cost;
+  final String cuisine;
+
+  FoodSpot({ 
+    required this.id, 
+    required this.name,
+    required this.imageUrl, 
+    required this.hours, 
+    required this.cost,
+    required this.cuisine
+  });
+}
