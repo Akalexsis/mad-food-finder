5e375d1ebf341b13409892d5eccc77c0f0dbf5e4
Kayla Thornton <kaylathornton55@icloud.com>
2026-03-12 18:25:21 -0400

    update: add text input to review screen

--- lib/screens/reviewScreen.dart
+++ lib/screens/reviewScreen.dart
@@ -17,10 +17,16 @@ class _ReviewScreenState extends State<ReviewScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _reviewController = TextEditingController();
 
-  // add review to database
+  // add review to database navigate back to previous page
   void addReview() {
+    // add review to database
 
+    const reviewMsg = SnackBar( content: Text( 'Your review has been added!' ) );
+    ScaffoldMessenger.of(context).showSnackBar(reviewMsg);
+
+    Navigator.pop(context);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,9 +34,30 @@ class _ReviewScreenState extends State<ReviewScreen> {
         child: Column(
           children: [
             Text('Add a Review!', style: TextStyle( fontSize: 24 ) ),
-            SizedBox( height: 30),
+            SizedBox( height: 30 ),
 
-            
+            Text('Review Name', textAlign: TextAlign.start),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Review Name'
+              ),
+            ),
+            SizedBox( height: 15 ),
+
+            // Text('Review Name', textAlign: TextAlign.start),
+            TextField(
+              controller: _reviewController,
+              decoration: InputDecoration(
+                labelText: 'Add your review ...'
+              ),
+            ),
+            SizedBox( height: 15 ),
+
+            ElevatedButton(
+              onPressed: () { addReview(); }, 
+              child: Text('Add Review')
+            )
           ]
         )
       )
