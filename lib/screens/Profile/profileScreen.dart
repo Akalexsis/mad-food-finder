45131edceba007dd439112863959a37fccfd9346
Kayla Thornton <kaylathornton55@icloud.com>
2026-03-14 18:10:05 -0400

    update: add layout to profile page

--- lib/screens/Profile/profileScreen.dart
+++ lib/screens/Profile/profileScreen.dart
@@ -15,10 +15,12 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   // store settings
-  late String? username;
-  late String? displayName;
-  late String? cuisine;
-  late double? budget;
+  late String username;
+  late String displayName;
+  late String cuisine;
+  late double budget;
+
+  int favorites = 3; // TO-DO get number of favoirted items
 
   // get settings
   @override
@@ -40,29 +42,61 @@ class _ProfileScreenState extends State<ProfileScreen> {
     });
   }
 
+  // TO-DO - ROUTE USER TO HOME PAGE AND DISPLAY LIST OF FAVORITED ITEMS
+  void _showFavorites() {
+    print('Favorite spots');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: .start,
         children: [
-          ElevatedButton(
-            onPressed: () async {
-              Navigator.push(
-                context,
-                MaterialPageRoute( builder: (context) => EditProfile() ),
-              );
+          
 
-              await _getPrefs(); // refresh page after saving profile edits
-            }, 
-            child: Text('Edit') 
-          ),
           // Render username, profile pic, number of favorites, and number of spots visited
-            // add edit profile button and view favorites button
-          Text('Name: $displayName'),
-          Text('Username: $username'),
-          Text('Monthly Budget: \$$budget'),
-          Text('Preferred Cuisine: $cuisine'),
+          Column( // group related information
+            children: [
+              // display user information
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(username, style: TextStyle( fontSize: 24 ), ),
+                subtitle: Row( 
+                  mainAxisAlignment: .spaceBetween,
+                  children: [
+                    Text('\$$budget Monthly Budget'),
+                    Text('$favorites Favorites'),
+                    Text('$cuisine Preferred Cuisine')
+                  ],
+                )
+              ),
+
+              // display buttons in a row
+              Row(
+                children: [
+                  ElevatedButton( // route to edit profile page to update preferences
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute( builder: (context) => EditProfile() ),
+                    );
+
+                    await _getPrefs(); // refresh page after saving profile edits
+                  }, 
+                  child: Text('Edit') 
+                ),
+
+
+                ElevatedButton( // route to home page to see list of favorited spots
+                  onPressed: () { _showFavorites(); },
+                  child: Text('Favorites') 
+                ),
+                ],
+              )
+              
+              ],
+            ),
           
 
           // List information to get from user (budget amount)
